f9ad9a09ca040ddec3518f17f83e1a366ddb8f65
Kharisma  Nur Hayati <kharisma1900016155@webmail.uad.ac.id>
2021-06-09 19:57:50 +0700

    commit pertama

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialDesign } from '../material/material.module';
+
+const routes: Routes =[
+{
+  path:'',
+  component:AdminComponent,
+  children:[
+    {
+      path:'dashboard',
+      component:DashboardComponent
+    },
+    {
+      path:'',
+      pathMatch:'full',
+      redirectTo:'/admin/dashboard'
+    }
+  ]
+  }
+]
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialDesign
+  ]
+})
+export class AdminModule { }

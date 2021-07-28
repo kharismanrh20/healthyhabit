819c3b1028526e67cf49f680ddb61690076d9a63
Kharisma  Nur Hayati <kharisma1900016155@webmail.uad.ac.id>
2021-07-28 18:24:41 +0700

    commit tujuh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAEg0i9-Kz5UtuEdFZYye3yCVS5ZK73NQc",
+    authDomain: "catatan-4b022.firebaseapp.com",
+    projectId: "catatan-4b022",
+    storageBucket: "catatan-4b022.appspot.com",
+    messagingSenderId: "343198986544",
+    appId: "1:343198986544:web:800cc0c76eb18d491318f5"
+    }
 };
